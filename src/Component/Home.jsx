c78eb9672f13630aef5c003296d5717e6cda84e4
Hypekage <jmestmnag@gmail.com>
2022-12-05 20:11:12 +0545

    create todo structure created

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,10 +1,15 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import image from "../Assests/profile.jpg";
 import HomeRoutes from "../Routes/HomeRoutes";
 
-
 const Home = () => {
+
+  const navigate = useNavigate()
+  const homeHandler = () => {
+    navigate('/');
+  }
+
   return (
     <>
       <div className="container">
@@ -12,39 +17,50 @@ const Home = () => {
           <div className="row">
             <div className="col-md-4">
               <div className="left-body">
-                <div className="profile-img">
+                <div className="profile-img" onClick={homeHandler}>
                   <img src={image} alt="profile" />
                 </div>
                 <p>Saroj-Ghalan</p>
                 <div className="left-body-content">
                   <ul>
                     <li>
-                      <NavLink to="/create-todo">
-                        <div className="button">
-                          <button>Create TODO</button>
-                        </div>
+                      <NavLink to="/create-todo" className="link-to">
+                        <span className="span-icon">
+                          <i class="fa-solid fa-plus"></i>
+                        </span>
+                        Create TODO
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink to="/all-todo" className="link-to">
+                        <span className="span-icon">
+                          <i class="fa-solid fa-folder"></i>
+                        </span>
+                        All <span className="span-count"></span>
                       </NavLink>
                     </li>
                     <li>
                       <NavLink to="/important-todo" className="link-to">
-                        Important <span></span>
+                        <span className="span-icon">
+                          <i class="fa-solid fa-bookmark"></i>
+                        </span>
+                        Important <span className="span-count"></span>
                       </NavLink>
                     </li>
                     <li>
                       <NavLink to="/favorite-todo" className="link-to">
-                        Favorite
+                        <span className="span-icon">
+                          <i class="fa-sharp fa-solid fa-heart"></i>
+                        </span>
+                        Favorite<span className="span-count"></span>
                       </NavLink>
-                      <span></span>
-                    </li>
-                    <li>
-                      <NavLink to="/all-todo" className="link-to">
-                        All
-                      </NavLink>
-                      <span></span>
                     </li>
                     <li>
                       <NavLink to="/completed-todo" className="link-to">
-                        Completed<span></span>
+                        <span className="span-icon">
+                          <i class="fa-solid fa-check"></i>
+                        </span>
+                        Completed<span className="span-count"></span>
                       </NavLink>
                     </li>
                   </ul>
@@ -52,9 +68,9 @@ const Home = () => {
               </div>
             </div>
             <div className="col-md-8">
-                <div className="right-body">
-                    <HomeRoutes />
-                </div>
+              <div className="right-body">
+                <HomeRoutes />
+              </div>
             </div>
           </div>
         </div>
--- src/Pages/SidePage/CreateTodo.jsx
+++ src/Pages/SidePage/CreateTodo.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const CreateTodo = () => {
+  const [startDate, setStartDate] = useState(new Date());
   return (
-    <div>CreateTodo</div>
-  )
-}
+    <>
+      <div className="body-content">
+        <div className="title">
+          <h4>Create Todo</h4>
+        </div>
+        <div className="create-main">
+          <div className="create-input">
+            <label htmlFor="">Topic : </label>
+            <br />
+            <input type="text" placeholder="topic here ..." />
+          </div>
+          <div className="create-input">
+            <label htmlFor="">About : </label>
+            <br />
+            <textarea
+              name=""
+              id=""
+              cols="60"
+              rows="2"
+              placeholder="write description about ..."
+            ></textarea>
+          </div>
+          <div className="create-input">
+            <label htmlFor="">Must Complete On : </label>
+            <br />
+            <DatePicker
+              selected={startDate}
+              onChange={(date: Date) => setStartDate(date)}
+            />
+          </div>
+          <div className="create-btn">
+            <button>Create TODO</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default CreateTodo
+export default CreateTodo;
